bc5fe87f7428b3f8447f0d21bc9af148ff83f516
Johnson Chen <chnejohnson@gmail.com>
2025-03-19 14:12:20 +0800

    dev: no ssl in connection with db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,41 +1,42 @@
-import { z } from "zod"
-import { createSqlTag } from "slonik"
-import Redis from "ioredis"
-import { createPool } from "slonik"
-import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation"
+import { z } from 'zod'
+import { createSqlTag } from 'slonik'
+import Redis from 'ioredis'
+import { createPool } from 'slonik'
+import { createFieldNameTransformationInterceptor } from 'slonik-interceptor-field-name-transformation'
 
 if (!process.env.REDIS_URL) {
-    throw Error("REDIS_URL env not set")
+	throw Error('REDIS_URL env not set')
 }
 export const redisClient = new Redis(process.env.REDIS_URL, {
-    maxRetriesPerRequest: null
+	maxRetriesPerRequest: null,
 })
 
 const interceptors = [
-    createFieldNameTransformationInterceptor({
-        format: "CAMEL_CASE"
-    })
+	createFieldNameTransformationInterceptor({
+		format: 'CAMEL_CASE',
+	}),
 ]
 
 const db = createPool(process.env.DB_URL!, {
-    interceptors,
-    // Only use SSL in production
-    ...(process.env.NODE_ENV === "production" 
-        ? {
-            ssl: {
-                rejectUnauthorized: true
-            }
-          } 
-        : {})
+	interceptors,
+	// Only use SSL in production
+	...(process.env.NODE_ENV === 'production'
+		? {
+				ssl: {
+					rejectUnauthorized: false,
+					require: false,
+				},
+		  }
+		: {}),
 })
 
 export const sql = createSqlTag({
-    typeAliases: {
-        id: z.object({
-            id: z.number()
-        }),
-        void: z.object({}).strict()
-    }
+	typeAliases: {
+		id: z.object({
+			id: z.number(),
+		}),
+		void: z.object({}).strict(),
+	},
 })
 
 export default db
